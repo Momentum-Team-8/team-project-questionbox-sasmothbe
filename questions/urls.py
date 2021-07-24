383e8d5893c0989b360134b4fdfc8f13595f678e
qbarefoot <quintenbarefoot@gmail.com>
2021-07-23 21:55:34 -0400

    added tag serializers, urls, and views, added search functions  to views

--- questions/urls.py
+++ questions/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.QuestionList.as_view(), name='question-list'),
     path('<int:pk>/', views.QuestionDetail.as_view(), name='question-detail'),
+    path('tag/', views.TagList.as_view(), name='tag-list'),
 ]
 
