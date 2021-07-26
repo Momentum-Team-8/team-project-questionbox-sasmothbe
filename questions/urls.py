e140b606ce6db20ad6500eaa7f5b5c0b80fca371
smilewendy7 <smilewendy7@gmail.com>
2021-07-25 22:37:40 -0400

    add create question url

--- questions/urls.py
+++ questions/urls.py
@@ -8,6 +8,7 @@ from questions import views
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.QuestionList.as_view(), name='question-list'),
+    path('create/', views.QuestionCreate.as_view(), name='create-question'),
     path('<int:pk>/', views.QuestionDetail.as_view(), name='question-detail'),
     path('tag/', views.TagList.as_view(), name='tag-list'),
     
